4eec5e1db6d4faf6a373598a1e8fe361bec99511
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2019-11-12 12:55:36 -0500

    Edit transcode script to keep original files and add create_target.sh

--- mini_dataset_to_transcode/scripts/transcode_scale_and_pad_all.sh
+++ mini_dataset_to_transcode/scripts/transcode_scale_and_pad_all.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-# this script is meant to be used with 'datalad run --explicit'
+# this script is meant to be used with 'datalad run'
 
-git rm -r --cached 000*
-mkdir tmp
-mv 000* tmp/
+pip install -r scripts/requirements_transcode_scale_and_pad.txt --upgrade
+if [ $? -ne 0 ]; then
+   echo "Failed to install requirements: pip install: $?"
+   return $?
+fi
 
 function scale_and_pad {
-  if [[ "$1" == "0000" ]] ; then
-    printf '\x00\x00\x00\x00\x00\x00\x00\x00' > tmp/$1/$2.JPEG.target
-  elif [[ "$1" == "0001" ]] ; then
-    printf '\x01\x00\x00\x00\x00\x00\x00\x00' > tmp/$1/$2.JPEG.target
-  elif [[ "$1" == "0002" ]] ; then
-    printf '\x02\x00\x00\x00\x00\x00\x00\x00' > tmp/$1/$2.JPEG.target
-  fi
-
   python -m pyheifconcat.image2mp4 \
          --codec=h265 --tile=512:512:yuv420 --crf=10 \
          --output=$2.mp4 \
          --primary --thumb --name=$2.JPEG \
-         --item=path=tmp/$1/$2.JPEG \
+         --item=path=$1/$2.JPEG \
          --hidden --name=target --mime=application/octet-stream \
-         --item=type=mime,path=tmp/$1/$2.JPEG.target
+         --item=type=mime,path=$1/$2.JPEG.target
 }
 
 scale_and_pad 0000 n02119789_4903
@@ -35,5 +29,3 @@ scale_and_pad 0001 n02100735_8211
 scale_and_pad 0002 n02110185_679
 scale_and_pad 0002 n02110185_2014
 scale_and_pad 0002 n02110185_7939
-
-rm -rf tmp
