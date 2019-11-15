f41541dec42b454e4278dae580fa12dbb1820f31
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2019-11-14 22:37:58 -0500

    Update pyheifconcat to fix the mdat box size

--- mini_dataset_to_concat/scripts/concat.sh
+++ mini_dataset_to_concat/scripts/concat.sh
@@ -3,9 +3,10 @@
 # this script is meant to be used with 'datalad run --explicit'
 
 pip install -r scripts/requirements_concat.txt --upgrade
-if [ $? -ne 0 ]; then
-   echo "Failed to install requirements: pip install: $?"
-   return $?
+ERR_CODE=$?
+if [ $ERR_CODE -ne 0 ]; then
+   echo "Failed to install requirements: pip install: $ERR_CODE"
+   exit $ERR_CODE
 fi
 
 python -m pyheifconcat.create_container concat.bzna
--- mini_dataset_to_concat/scripts/index_metadata.sh
+++ mini_dataset_to_concat/scripts/index_metadata.sh
@@ -3,9 +3,10 @@
 # this script is meant to be used with 'datalad run --explicit'
 
 pip install -r scripts/requirements_index_metadata.txt --upgrade
-if [ $? -ne 0 ]; then
-   echo "Failed to install requirements: pip install: $?"
-   return $?
+ERR_CODE=$?
+if [ $ERR_CODE -ne 0 ]; then
+   echo "Failed to install requirements: pip install: $ERR_CODE"
+   exit $ERR_CODE
 fi
 
 [ -f concat_indexed.bzna ] || cp concat.bzna concat_indexed.bzna
